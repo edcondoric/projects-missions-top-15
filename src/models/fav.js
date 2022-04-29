35e6c3e77af0fd63a00b515c7c2fd55530f59783
edcondoric <edcondoriccora@gmail.com>
2022-04-29 18:06:51 -0500

    Adding struture of Articles on DB

--- src/models/fav.js
+++ src/models/fav.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose')
 
 const Fav = mongoose.model('Fav', {
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
     description: {
         type: String,
         required: true,
         trim: true
     },
-    completed: {
-        type: Boolean,
-        default: false
+    link: {
+        type: String,
+        required: true,
+        trim: true
     },
     owner: {
         type: mongoose.Schema.Types.ObjectId,
